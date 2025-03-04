7383e95f5afea55ccde4c2cec5790f90c0d632ff
Felipe Franco <felipejf739@hotmail.com>
2025-03-04 19:33:42 -0300

    alt

--- animeAllStar-back/animeAllStar-back/src/main/java/com/example/animeAllStar_back/model/entity/Player.java
+++ animeAllStar-back/animeAllStar-back/src/main/java/com/example/animeAllStar_back/model/entity/Player.java
@@ -2,7 +2,7 @@ package com.example.animeAllStar_back.model.entity;
 
 import javax.persistence.*;
 
-import com.example.animeAllStar_back.model.entity.Mundo.Mundo;
+
 import lombok.AllArgsConstructor;
 
 import lombok.Data;
@@ -13,13 +13,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
+
 public class Player extends Usuario {
 
     private String nickname;
     private String sexo;
 
-    @ManyToOne
-    @JoinColumn(name = "mundo_id")
-    private Mundo mundo;
+
 }
